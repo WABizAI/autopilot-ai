82d0529564e5763e11907cb4ed350958f75cc78c
WABizAI <info.faisaloffical@gmail.com>
2026-08-26 09:23:14 +0500

    Create App component for AutoPilot AI

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>AutoPilot AI</h1>
+      <p>Your AI Social Media Manager</p>
+    </div>
+  );
+}
+
+export default App;
